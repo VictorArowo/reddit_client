570472095722a6dc1b8ba8a68630c0b7f8756276
Victor Arowo <arowov@gmail.com>
2019-09-13 17:28:24 +0100

    Added Reusable Components

---
+++ src/components/~reusables/variables/colors.js
@@ -0,0 +1,7 @@
+//====== COLOR VARIABLES ======//
+export const white = '#FFFFFF';
+export const theme_dark = '#28333F';
+export const theme_light = '#F9F9F9';
+export const theme_primary = '#FF4300';
+export const theme_secondary = '#0079D3';
+export const theme_failure = '#D03A30';
